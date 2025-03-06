73d15a133af332c91bf49180aafd41bd20019cdf
joshua23tech <joshua23.tech@gmail.com>
2025-03-06 23:55:18 +0000

    extended routes and crud operations to include create and update

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "restaurants", to: "restaurants#index"
+
+  get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create"
+
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+  get "restaurants/:id/edit", to: "restaurants#edit"
+  patch "restaurants/:id", to: "restaurants#update"
 end
